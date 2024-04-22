ae5ba2b23adbb8d863f93d548c4f71c18f801b6f
Garv2003 <aggarwalgarv0505@gmail.com>
2024-04-22 23:07:00 +0530

    Atmos

---
+++ theme/index.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  bgWhite: (opacity: number) => `rgba(255,255,255, ${opacity})`,
+};
+
+
+// name:#DEDAD7,#ff6cab
+
+// bgcolor : #575C5F,#000000
+
+// icon color :#B4BFC5,#ff6cab
+
+// container color : #DEDAD7,#ff6cab
